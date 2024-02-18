e989cc1fff8576300154ff3865bf76549c5551c1
armin-164 <arminsadr164@hotmail.com>
2024-02-18 22:02:34 +0100

    feat: add basic template that retrieves user from MySQL db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../lib/connect');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res) => {
+ connection.connect((err) => {
+  if (err) console.log('str', err);
+
+  let query = 'SELECT * FROM users';
+
+  connection.query(query, (err, data) => {
+    if (err) console.log('err', err);
+    res.json(data);
+  })
+ })
+})
 
 module.exports = router;
