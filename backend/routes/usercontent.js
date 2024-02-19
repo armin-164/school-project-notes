6d94c2dd84fa142ef3f91611a4b3e5414515485b
armin-164 <arminsadr164@hotmail.com>
2024-02-19 23:36:41 +0100

    feat: add GET request to fetch all notes connected to specific user

--- backend/routes/usercontent.js
+++ backend/routes/usercontent.js
@@ -2,4 +2,19 @@ var express = require('express');
 var router = express.Router();
 const connection = require('../lib/connect');
 
+router.get('/notes/:id', (req, res) => {
+    let user = req.params.id;
+
+    let query = 'SELECT * FROM notes WHERE User = ?';
+    connection.query(query, [user], (err, data) => {
+        if (err) console.log('err', err);
+        
+        if (data.length < 1) {
+            return res.json({ message: 'Data not found'})
+        }
+        res.json(data);
+      })
+
+})
+
 module.exports = router;
