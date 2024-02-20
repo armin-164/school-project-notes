e5d08be2b50d7ff5077b50628b2d36bd04bcdfc4
armin-164 <arminsadr164@hotmail.com>
2024-02-20 16:13:07 +0100

    feat: add function that retrieves user notes via GET request and creates elements for them

--- frontend/scripts/mainContent.js
+++ frontend/scripts/mainContent.js
@@ -1,3 +1,44 @@
+function createElementsForNotes() {
+    const user = localStorage.getItem('user');
+
+    if (user) {
+        fetch(`http://localhost:3000/content/notes/${user}`)
+        .then(res => res.json())
+        .then(data => {
+            const noteContainer = document.querySelector('.note-container');
+
+            data.forEach(note => {
+
+                const noteCard = document.createElement('div');
+            noteCard.classList.add('note-card');
+
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = note.Title;
+
+            const contentElement = document.createElement('p');
+            contentElement.textContent = note.Content;
+
+            const categoryElement = document.createElement('p');
+            categoryElement.textContent = `Category: ${note.Category}`;
+
+            const lastUpdateAtElement = document.createElement('p');
+            lastUpdateAtElement.innerHTML = `
+            <span class="material-symbols-outlined">schedule</span>
+            Last Update: ${new Date(note.LastUpdateAt).toLocaleDateString()}`;
+
+            noteCard.appendChild(titleElement);
+            noteCard.appendChild(contentElement);
+            noteCard.appendChild(categoryElement);
+            noteCard.appendChild(lastUpdateAtElement);
+
+            noteContainer.appendChild(noteCard);
+
+            })
+        })
+    }
+}
+
+
 function displayMainContent() {
     const mainElement = document.querySelector('main');
     mainElement.innerHTML = `
@@ -13,6 +54,7 @@ function displayMainContent() {
             </div>
         </div>
     `;
+    createElementsForNotes();
 }
 
 
