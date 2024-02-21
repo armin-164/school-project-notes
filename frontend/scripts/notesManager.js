8c6547a0f4e0882a2815b05144085a906a833cd2
armin-164 <arminsadr164@hotmail.com>
2024-02-21 18:34:00 +0100

    feat: add function that inserts note in DB

--- frontend/scripts/notesManager.js
+++ frontend/scripts/notesManager.js
@@ -34,6 +34,24 @@ function createNote() {
 }
 
 
+function addNoteToDatabase() {
+    const noteData = {
+        User: localStorage.getItem('user'),
+        Title: document.querySelector('.note-title').value,
+        Content: document.getElementById('note-content').value
+    }
+    
 
+    fetch('http://localhost:3000/content/notes/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(noteData)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+}
 
 export default createNote;
