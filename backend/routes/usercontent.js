83d171a5e94be3570557a5c328435e523d04aba6
armin-164 <arminsadr164@hotmail.com>
2024-02-21 18:43:45 +0100

    feat: add POST request to insert into notes and send back NoteID

--- backend/routes/usercontent.js
+++ backend/routes/usercontent.js
@@ -17,4 +17,28 @@ router.get('/notes/:id', (req, res) => {
 
 })
 
+router.post('/notes/add', (req, res) => {
+
+    const insertQuery = 'INSERT into notes (User, Title, Content) VALUES (?, ?, ?)';
+    const selectQuery = 'SELECT LAST_INSERT_ID() as NoteID'; 
+
+    connection.query(insertQuery, [req.body.User, req.body.Title, req.body.Content], (err, data) => {
+        if (err) {
+            console.log('err', err);
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+
+        connection.query(selectQuery, (err, data) => {
+            if (err) {
+                console.log('err', err);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+            
+            const noteID = data[0].NoteID; 
+            return res.status(201).json({ message: 'User added successfully', NoteID: noteID });
+        });
+    });
+})
+
+
 module.exports = router;
